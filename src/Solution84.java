104743289609ce407e75e401ab2c2f51f9a4f0a9
lichenxiong <2251727682@qq.com>
2022-02-06 08:47:25 +0800

    leetcode

---
+++ src/Solution84.java
@@ -0,0 +1,12 @@
+public class Solution84 {
+    public int largestRectangleArea(int[] heights) {
+
+
+        return 1;
+    }
+
+
+
+
+
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+    public static void main(String[] args) {
+        Solution84 solution84 = new Solution84();
+
+
+    }
+}
