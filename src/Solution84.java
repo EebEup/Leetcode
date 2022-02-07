3f0c46e53ab8b18b716c8fa1b7ec9def3f3ceffc
lichenxiong <2251727682@qq.com>
2022-02-07 14:57:28 +0800

    lcx

--- src/Solution84.java
+++ src/Solution84.java
@@ -1,9 +1,64 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Solution84 {
+    //暴力解法
+    /*
     public int largestRectangleArea(int[] heights) {
+        int max=0;
 
+        for(int i=0;i<heights.length;i++){
+            max=Math.max(max,(getr(heights,i)-getl(heights,i)+1)*heights[i]);
+        }
+    return max;
 
-        return 1;
     }
+    int getl(int[] heights,int i){
+        for(int j=i;j>=0;j--){
+            if(heights[j]<heights[i]) return j+1;
+        }
+        return 0;
+    }
+    int getr(int[] heights,int i){
+        for(int j=i;j<heights.length;j++) {
+            if (heights[j] < heights[i]) {
+                return j - 1;
+            }
+        }
+        return heights.length-1;
+    }
+
+    */
+
+
+        public int largestRectangleArea(int[] heights) {
+            int n = heights.length;
+            int[] left = new int[n];
+            int[] right = new int[n];
+            Deque<Integer> mono_stack = new ArrayDeque<Integer>();
+            for (int i = 0; i < n; ++i) {
+                while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                    mono_stack.pop();
+                }
+                left[i] = (mono_stack.isEmpty() ? -1 : mono_stack.peek());
+                mono_stack.push(i);
+            }
+
+            mono_stack.clear();
+            for (int i = n - 1; i >= 0; --i) {
+                while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                    mono_stack.pop();
+                }
+                right[i] = (mono_stack.isEmpty() ? n : mono_stack.peek());
+                mono_stack.push(i);
+            }
+
+            int ans = 0;
+            for (int i = 0; i < n; ++i) {
+                ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+            }
+            return ans;
+        }
 
 
 
--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,9 @@ public class Test {
 
     public static void main(String[] args) {
         Solution84 solution84 = new Solution84();
-
+        int[] heights = new int[]{2,1,5,6,2,3};
+        int max = solution84.largestRectangleArea(heights);
+        System.out.println(max);
 
     }
 }
