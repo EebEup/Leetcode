1dcc0813cc295c5b2de3f0dd70d7d669a2e260ef
lichenxiong <2251727682@qq.com>
2022-02-09 09:32:52 +0800

    lcx

---
+++ src/Solution94.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+
+
+public class Solution94 {
+    List<Integer> res =new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        f(root);
+        return res;
+    }
+    void f(TreeNode a){
+        if(a==null) return;
+        f(a.left);
+        res.add(a.val);
+        f(a.right);
+
+    }
+
+
+}
