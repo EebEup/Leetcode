95ebd9a347d9a995ef70225cce17d3d19d7751ff
lichenxiong <2251727682@qq.com>
2022-02-09 15:43:38 +0800

    lcx

--- src/Solution102.java
+++ src/Solution102.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Solution102 {
     List<List<Integer>> res =new ArrayList<>();
@@ -30,5 +28,35 @@ public class Solution102 {
 
 
     }
+
+
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return ret;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();
+            int currentLevelSize = queue.size();
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ret.add(level);
+        }
+
+        return ret;
+    }
+
+
 
 }
---
+++ src/Solution104.java
@@ -0,0 +1,18 @@
+public class Solution104 {
+    int ans=1;
+    public int maxDepth(TreeNode root) {
+
+        travel(root,0);
+        return  ans;
+    }
+
+    void travel(TreeNode t,int depth){
+        if(t==null) return;
+        depth++;
+        ans=Math.max(ans,depth);
+        travel(t.left,depth);
+        travel(t.right,depth);
+
+    }
+
+}
